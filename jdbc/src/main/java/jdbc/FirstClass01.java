0d65ad6e48e6cf2b48cff2b159c5d08b6320fda8
Nikhil Kadam <83809232nk@gmail.com>
2023-03-12 18:15:23 +0530

    commit if else condition in FirstClass jdbc project

--- jdbc/src/main/java/jdbc/FirstClass01.java
+++ jdbc/src/main/java/jdbc/FirstClass01.java
@@ -1,10 +1,16 @@
 package jdbc;
 
 public class FirstClass01 {
+	static int a = 3;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st change");
+		if (a == 3) {
+			System.out.println("Ans is " + a);
+		} else {
+			System.out.println("wrong ans");
+		}
 	}
 
 }
