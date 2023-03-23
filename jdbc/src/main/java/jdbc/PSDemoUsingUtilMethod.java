bb03461c93c53a6d6e0ed7d8f0a6a673abaac3d0
Nikhil Kadam <83809232nk@gmail.com>
2023-03-23 19:31:31 +0530

    New Code Added for CRUD

---
+++ jdbc/src/main/java/jdbc/PSDemoUsingUtilMethod.java
@@ -0,0 +1,33 @@
+package jdbc;
+
+import com.pojo.EmployeeDetails;
+
+public class PSDemoUsingUtilMethod {
+
+	public static void main(String[] args) {
+
+		CRUDDemo cd = new CRUDDemo();
+		// Read Employee table
+		System.out.println("-----------READ Operation------------");
+		//cd.readEmployee();
+		
+		// Insert Employee
+		System.out.println("-----------Write Operation------------");
+		EmployeeDetails emp = new EmployeeDetails();
+		emp.setEmpid(12);
+		emp.setEmpname("abcd");
+		emp.setEmpcity("Mumbai");
+		//cd.createEmployee(emp);
+
+		System.out.println("-----------Update Operation------------");
+		EmployeeDetails empU = new EmployeeDetails();
+		empU.setEmpid(14);
+		empU.setEmpname("Updated EMployee");
+		//cd.updateEmployee(empU);
+		
+		
+		cd.deleteEmployee(4);
+		
+		
+	}
+}
