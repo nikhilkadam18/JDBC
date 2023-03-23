22d045bd210ecd6d727c1e6031b9df19a95a602f
Nikhil Kadam <83809232nk@gmail.com>
2023-03-23 19:21:22 +0530

    Added New Code for Prepared Statement

--- jdbc/src/main/java/jdbc/FirstClass01.java
+++ jdbc/src/main/java/jdbc/FirstClass01.java
@@ -1,16 +1,38 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class FirstClass01 {
-	static int a = 3;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("1st change");
-		if (a == 3) {
-			System.out.println("Ans is " + a);
-		} else {
-			System.out.println("wrong ans");
-		}
-	}
 
+		try {
+			// Step 1 Load the driver 
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			// Step 2 Connect with db using url user and pass
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/btm", "root", "admin");
+			
+			// Step 3 create statement
+			Statement stmt = con.createStatement();
+
+			// Step 4 execute query
+			ResultSet rs = null;
+			
+			rs = stmt.executeQuery("SELECT * FROM btm.employeedetails where empid=1");
+
+			System.out.println(rs);
+			
+			// extract data 
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) );
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
 }
---
+++ jdbc/src/main/java/jdbc/PSDemo.java
@@ -0,0 +1,41 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PSDemo {
+
+	public static void main(String[] args) {
+
+		try {
+			// Step 1 Load the driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// Step 2 Connect with db using url user and pass
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/btm", "root", "admin");
+
+			System.out.println("connection successful");
+
+			// Step 3 create statement
+			PreparedStatement ps = con
+					.prepareStatement("SELECT * FROM btm.employeedetails where empid=? AND empname = ? ");
+			ps.setInt(1, 2);
+			ps.setString(2, "Ram");
+
+			ResultSet rs = ps.executeQuery();
+
+			System.out.println("Query executed");
+			while (rs.next())
+				
+			{
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			}
+			con.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+}
